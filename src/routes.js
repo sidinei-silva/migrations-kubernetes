bb341cd492caeb2c0470a68430115125a57e24f0
Sidinei Silva <sidinei.silva02@gmail.com>
2020-07-20 12:23:21 -0300

    Forçando teste Job Ok e Deployment fails

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const routes = new Router();
 
 routes.get('/', (req, res) => {
   const env_teste = process.env.HELLO_ENV ?? '';
-  res.json({ message: 'Teste 08', env: env_teste });
+  res.json({ message: 'Testando Job Ok e Deployment Fails', env: env_teste });
 });
 
 export default routes;
