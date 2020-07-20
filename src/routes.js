b8a3ea0564c08843b79853ca7263ee8177e0c8b9
Sidinei Silva <sidinei.silva02@gmail.com>
2020-07-20 14:10:43 -0300

    Testando migration sem job

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const routes = new Router();
 routes.get('/', (req, res) => {
   const env_teste = process.env.HELLO_ENV ?? '';
   res.json({
-    message: 'Testando Job Fails e Deployment Ok',
+    message: 'Teste sem job, com livenessProbe',
     env: env_teste,
   });
 });
