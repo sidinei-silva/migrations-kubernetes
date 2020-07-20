d70545433b3556cdef384536887fc9a5729df7a1
Sidinei Silva <sidinei.silva02@gmail.com>
2020-07-20 16:34:36 -0300

    Retornando job tudo ok

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const routes = new Router();
 routes.get('/', (req, res) => {
   const env_teste = process.env.HELLO_ENV ?? '';
   res.json({
-    message: 'Job com init container tudo ok',
+    message: 'Retornando job tudo ok',
     env: env_teste,
   });
 });
