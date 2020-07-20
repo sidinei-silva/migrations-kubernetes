70c02a92f982a0e627f0ab8a26d1650f4aadace3
Sidinei Silva <sidinei.silva02@gmail.com>
2020-07-20 15:05:29 -0300

    Testando tudo ok

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const routes = new Router();
 routes.get('/', (req, res) => {
   const env_teste = process.env.HELLO_ENV ?? '';
   res.json({
-    message: 'Teste com livenessProbe, com erro no comando migrate',
+    message: 'Tudo ok',
     env: env_teste,
   });
 });
