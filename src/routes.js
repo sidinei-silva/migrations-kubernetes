5acaaa5f9624cb7dfe071d3311bd805bea6dc947
Sidinei Silva <sidinei.silva02@gmail.com>
2020-07-20 16:39:24 -0300

    Forçando falha do job

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const routes = new Router();
 routes.get('/', (req, res) => {
   const env_teste = process.env.HELLO_ENV ?? '';
   res.json({
-    message: 'Retornando job tudo ok',
+    message: 'Retornando job falhando',
     env: env_teste,
   });
 });
