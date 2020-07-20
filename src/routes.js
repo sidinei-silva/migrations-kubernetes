de3e57fd807ee53a97de40e0d8ba105b2b9a57bc
Sidinei Silva <sidinei.silva02@gmail.com>
2020-07-20 14:32:46 -0300

    Mensagem de retorno

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const routes = new Router();
 routes.get('/', (req, res) => {
   const env_teste = process.env.HELLO_ENV ?? '';
   res.json({
-    message: 'Teste sem job, com livenessProbe',
+    message: 'Teste sem job, com livenessProbe, com tudo ok',
     env: env_teste,
   });
 });
